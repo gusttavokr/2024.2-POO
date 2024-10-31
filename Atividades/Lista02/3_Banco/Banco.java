6bb12eeae4c15b435fc877ae3add60ec2bf0fc55
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-10-31 11:15:48 +0000

    banco

--- Atividades/Lista02/3_Banco/Banco.java
+++ Atividades/Lista02/3_Banco/Banco.java
@@ -1,7 +1,7 @@
 public class Banco {
     private String titular;
     private int numConta;
-    private float saldo;
+    private double saldo = 0.00;
 
     public String gettitular(){
         return this.titular;
@@ -9,9 +9,36 @@ public class Banco {
     public int getnumConta(){
         return this.numConta;
     }
-    public float getsaldo(){
+    public double getsaldo(){
         return this.saldo;
     }
 
-    public void settitular()
+    public void settitular(String t){
+        this.titular = t;
+    }
+    public void setnumConta(int c){
+        this.numConta = c;
+    }
+    public void setsaldo(double s){
+        this.saldo = s;
+    }
+
+    public double depositar(double deposito){
+        setsaldo(deposito + getsaldo());
+        System.out.println(String.format("Você depositou %.2f, seu saldo agora é: %.2f", deposito, getsaldo()));
+        return getsaldo();
+    }
+
+    public double sacar(double saque){
+        if (saque > getsaldo()) {
+            System.out.println("Você não pode sacar esse valor seu porra!");
+            saque = 0;
+            sacar(saque);
+        }
+        else {
+            setsaldo(getsaldo()- saque);
+            System.out.println(String.format("Você sacou %.2f, seu saldo agora é: %.2f", saque, getsaldo()));
+        }
+        return getsaldo();
+    }
 }
--- Atividades/Lista02/3_Banco/UI.java
+++ Atividades/Lista02/3_Banco/UI.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class UI {
     public static void main(String[] args) {
-        
+        Banco c1 = new Banco();
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o nome do titular:");
+        c1.settitular(leitor.nextLine());
+        System.out.println("Digite o número da conta");
+        c1.setnumConta(leitor.nextInt());
+
+        System.out.println("Deposite um valor:");
+        c1.depositar(leitor.nextDouble());
+        System.out.println("Saque um valor:");
+        c1.sacar(leitor.nextDouble());
+        leitor.close();
     }
 }
