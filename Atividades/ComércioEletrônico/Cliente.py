d737d8d253c7f583bd0095bc4f877bdabe77b510
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-11-16 22:24:22 +0000

    Categoria

--- Atividades/ComércioEletrônico/Cliente.py
+++ Atividades/ComércioEletrônico/Cliente.py
@@ -2,45 +2,45 @@ import json
 
 class Cliente:
     def __init__(self, id, nome, email, fone): # Método construtor
-        self.id = id
-        self.nome = nome
-        self.email = email
-        self.fone = fone
+        self.__id = id
+        self.__nome = nome
+        self.__email = email
+        self.__fone = fone
 
-    # def getId(self):
-    #     return self.__id
-    # def setId(self, id):
-    #     if len(id) > 0:
-    #         self.__id = id
-    #     else:
-    #         raise ValueError('Id inválido')
+    def getId(self):
+        return self.__id
+    def setId(self, id):
+        if len(str(id)) > 0:
+            self.__id = id
+        else:
+            raise ValueError('Id inválido')
 
-    # def getNome(self):
-    #     return self.__nome
-    # def setNome(self, nome):
-    #     if len(nome) > 0:
-    #         self.__nome = nome
-    #     else:
-    #         raise ValueError('Nome inválido')
+    def getNome(self):
+        return self.__nome
+    def setNome(self, nome):
+        if len(nome) > 0:
+            self.__nome = nome
+        else:
+            raise ValueError('Nome inválido')
 
-    # def getEmail(self):
-    #     return self.__email
-    # def setEmail(self, email):
-    #     if len(email) > 0:
-    #         self.__email = email
-    #     else:
-    #         raise ValueError('Email inválido')
+    def getEmail(self):
+        return self.__email
+    def setEmail(self, email):
+        if len(email) > 0:
+            self.__email = email
+        else:
+            raise ValueError('Email inválido')
 
-    # def getFone(self):
-    #     return self.__fone
-    # def setFone(self, fone):
-    #     if len(fone) > 0:
-    #         self.__fone = fone
-    #     else:
-    #         raise ValueError('Fone inválido')
+    def getFone(self):
+        return self.__fone
+    def setFone(self, fone):
+        if len(fone) > 0:
+            self.__fone = fone
+        else:
+            raise ValueError('Fone inválido')
 
     def __str__(self): # Formatação do terminal
-        return f'{self.id} - {self.nome} - {self.email} - {self.fone}'
+        return f'{self.getId()} - {self.getNome()} - {self.getEmail()} - {self.getFone()}'
     
 class Clientes:
     objetos = [] # Lista de Objetos, (no caso clientes), que serão usados
@@ -50,9 +50,9 @@ class Clientes:
         cls.abrir() # Abrindo o arquivo json, onde está registrado os clientes
         id = 0 # O id começa em zero
         for x in cls.objetos: # Percorrendo a lista de objetos
-            if x.id > id: # Se o x for maior que o id
-                id = x.id # Ele se tornará o x.id
-        obj.id = id + 1 # O id do objeto será o maior, mais um
+            if x.getId() > id: # Se o x for maior que o id
+                id = x.getId() # Ele se tornará o x.id
+        obj.setId(id+1) # O id do objeto será o maior, mais um
         cls.objetos.append(obj) 
         cls.salvar() # Use o método para salvar a alteração
 
@@ -72,7 +72,7 @@ class Clientes:
             with open ("Atividades/ComércioEletrônico/clientela.json", mode="r") as arquivo:
                 objetos_json = json.load(arquivo) # Criando uma variável para carregar o arquivo
                 for obj in objetos_json: # Iterando os atributos da variável
-                    c = Cliente(obj["id"], obj["nome"],obj["email"],obj["fone"]) # Instanciando os atributos da classe Cliente à uma variável
+                    c = Cliente(obj["_Cliente__id"], obj["_Cliente__nome"],obj["_Cliente__email"],obj["_Cliente__fone"]) # Instanciando os atributos da classe Cliente à uma variável
                     cls.objetos.append(c) # Adicionando a variável na lista dos objetos
         except FileNotFoundError:
             pass
@@ -85,15 +85,15 @@ class Clientes:
     @classmethod
     def listar_id(cls, id): # Método para retornar o id da lista
         for x in cls.objetos:
-            if x.id == id: # Se x for igual ao id, retorne x
+            if x.getId() == id: # Se x for igual ao id, retorne x
                 return x
         return None 
 
     @classmethod
     def atualizar(cls, obj):
-        x = cls.listar_id(obj.id) # Chamando a lista de id
+        x = cls.listar_id(obj.getId()) # Chamando a lista de id
         if x != None: # Se for diferente de nada
-            x.nome = obj.nome # Receba os novos atributos
-            x.email = obj.email
-            x.fone = obj.fone
+            x.setNome(obj.getNome())
+            x.setEmail(obj.getEmail())
+            x.setFone(obj.getFone())
             cls.salvar()
--- Atividades/ComércioEletrônico/UI.py
+++ Atividades/ComércioEletrônico/UI.py
@@ -1,8 +1,9 @@
 from Cliente import Cliente, Clientes # Importando as classes
+from Categoria import Categoria, Categorias
 
 class UI: # Interface
     def menu(): #Menu 
-        print("1- Novo cliente, 2- Listar Clientes\n3- Atualizar clientes, 9- Fim")
+        print("\nBEM VINDO AO SEU COMÉRCIO ELETRÔNICO\nOPÇÕES:\n1- Novo cliente, 2- Listar Clientes, 3- Atualizar clientes\n4- Inserir categoria, 5- Listar categorias, 6- Atualizar categorias\n9- Encerrar")
         return int(input("Digite uma opção: "))
     
     def main(): #Principal
@@ -15,13 +16,21 @@ class UI: # Interface
                 UI.listar_clientes() # Se for 2, liste os clientes
             if op == 3:
                 UI.atualizar_clientes() # Se for 3, atualize os clientes
+            if op == 4:
+                UI.inserir_categoria()
+            if op == 5:
+                UI.listar_categorias()
+            if op == 6:
+                UI.atualizar_categorias()
 
-    @classmethod
-    def inserir_cliente(cls): # Criando um cliente
+# FUNÇÕES PARA CLIENTES
+
+    @staticmethod
+    def inserir_cliente(): # Criando um cliente
         nome = input("Digite o nome: ")
         email = input("Digite o email: ")
         fone = input("Digite o fone: ")
-        cliente = Cliente(0, nome, email, fone) # Variável que recebe os atributos do novo cliente, instânciando na classe Cliente
+        cliente = Cliente(1, nome, email, fone) # Variável que recebe os atributos do novo cliente, instânciando na classe Cliente
         Clientes.inserir(cliente) # Inserindo a variável na lista de clientes com o método Inserir da classe Cliente
 
     @classmethod
@@ -43,6 +52,31 @@ class UI: # Interface
         cliente = Cliente(id, nome, email, fone)
         Clientes.atualizar(cliente) # Usando o método atualizar na variável cliente
 
+
+# FUNÇÕES PARA CATEGORIA
+
+    @classmethod
+    def inserir_categoria(cls):
+        Descrição = input("Digite a descrição da categoria: ")
+        ct = Categoria(1, Descrição)
+        Categorias.inserir(ct)
+    
+    @classmethod
+    def listar_categorias(cls):
+        categorias = Categorias.listar()
+        if len(categorias) == 0:
+            print("Nenhuma categoria cadastrada")
+        else:
+            for categoria in categorias:
+                print(categoria)
+    
+    @classmethod
+    def atualizar_categorias(cls):
+        UI.listar_categorias
+        id = int(input("Digite o id da categoria a ser alterada: "))
+        Descrição = input("Digite a nova descrição: ")
+        categoria = Categoria(id, Descrição)
+        Categorias.atualizar(categoria)
 
 
 UI.main() # Executando a interface
